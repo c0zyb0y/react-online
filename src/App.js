eee008702db9d83aa7ce9b73217a8d9bd2a24a57
c0zyb0y <no.phupha_st@tni.ac.th>
2021-12-18 09:37:46 +0700

    React router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import HomePage from "./Pages/HomePage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import AboutPage from "./Pages/AboutPage";
 
 
 function App() {
   return (
-    <>
+    <Router>
        <NavBar/>
-       <HomePage/>
+       <Switch>
+         <Route exact path ='/'><HomePage/></Route>
+         <Route path = '/about'><AboutPage/></Route>
+       </Switch>
        <Footer/>
-    </>
+    </Router>
   );
 }
 
